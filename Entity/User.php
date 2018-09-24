eab7ba62c06ff042ff02dc287de51e765c645bee
Gnagne Cedric <cecenho55@gmail.com>
2018-09-24 17:58:34 +0000

    Updated user table name

--- Entity/User.php
+++ Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
   /**
   * User
   *
-  * @ORM\Table(name="user")
+  * @ORM\Table(name="connect_user")
   * @ORM\Entity(repositoryClass="Puzzle\ConnectBundle\Repository\UserRepository")
   */
   class User implements AdvancedUserInterface, \Serializable, EquatableInterface
